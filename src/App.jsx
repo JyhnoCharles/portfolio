25d7c0937beea0ee80588b1ae986133b8b066ee8
JyhnoCharles <149640365+JyhnoCharles@users.noreply.github.com>
2026-01-21 13:27:12 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Waves from './assets/components/waves'
+
+function App() {
+  
+
+  return (
+   <main>
+
+        <Waves />
+
+          <h1>Jyhno Pierre Charles</h1>
+          <p>Software Developer </p>
+
+          
+   </main>
+  )
+}
+
+export default App
